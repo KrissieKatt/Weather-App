1ac5e0ccd763c00c4219f3c8342b7c59d84ddf5e
Krissie Katt <124374091+KrissieKatt@users.noreply.github.com>
2023-10-25 09:15:14 -0400

    began the process of the weather app , stopped at working on the background

---
+++ Weather App/Managers/Location Manager.swift
@@ -0,0 +1,8 @@
+//
+//  Location Manager.swift
+//  Weather App
+//
+//  Created by Christen Nancy Knight on 10/25/23.
+//
+
+import Foundation
---
+++ Weather App/Views/Welcome View .swift
@@ -0,0 +1,18 @@
+//
+//  Welcome View .swift
+//  Weather App
+//
+//  Created by Christen Nancy Knight on 10/25/23.
+//
+
+import SwiftUI
+
+struct Welcome_View_: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Welcome_View_()
+}
