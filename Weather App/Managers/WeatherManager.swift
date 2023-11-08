e0d6119165f1c511306609ef689ce5b775efc17e
Krissie Katt <124374091+KrissieKatt@users.noreply.github.com>
2023-11-08 10:49:36 -0500

    created a Loading View as well as integrading the API and corrdinates for weather location

---
+++ Weather App/Managers/WeatherManager.swift
@@ -0,0 +1,8 @@
+//
+//  WeatherManager.swift
+//  Weather App
+//
+//  Created by Christen Nancy Knight on 11/8/23.
+//
+
+import Foundation
---
+++ Weather App/Views/LoadinView.swift
@@ -0,0 +1,18 @@
+//
+//  LoadinView.swift
+//  Weather App
+//
+//  Created by Christen Nancy Knight on 11/8/23.
+//
+
+import SwiftUI
+
+struct LoadinView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoadinView()
+}
